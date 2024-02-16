289eda9a33d158e525330357847eb8598e5f07bd
Himansh Mudigonda <himudigonda@HimanshsMacBook.lan>
2024-02-15 20:06:25 -0700

    added toggleterm

--- init.lua
+++ init.lua
@@ -8,6 +8,14 @@ end
 
 require("core.utils").load_mappings()
 
+-- Add your mappings here
+vim.api.nvim_set_keymap('n', '<C-K><C-T>', ':TagbarToggle<CR>', {silent = true, noremap = true})
+vim.api.nvim_set_keymap('n', '<C-K><C-F>', ':NvimTreeToggle<CR>', {silent = true, noremap = true})
+vim.api.nvim_set_keymap('n', '<C-K><C-P>', ':Telescope find_files<CR>', {silent = true, noremap = true})
+vim.api.nvim_set_keymap('n', '<C-K><C-G>', ':Telescope live_grep<CR>', {silent = true, noremap = true})
+vim.api.nvim_set_keymap('n', '<C-T><C-T>', ':ToggleTerm<CR>', {silent = true, noremap = true})
+
+
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
 -- bootstrap lazy.nvim!
